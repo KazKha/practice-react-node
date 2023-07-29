3c5005200dc0bdaf19f7cedba7d5a9ec677a54f3
kazim <kazkha@gmail.com>
2023-07-29 17:03:21 +0530

    backend api expresjs

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,32 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const app = express();
-const PORT =  3300;
+const dotenv = require("dotenv").config();
+
+const {
+  ValidateEmail,
+  ValidateName,
+  ValidateMobiles,
+  genrateResposne 
+} = require("./utils/ReqValidation");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT;
 app.use(bodyParser.json());
 
-app.use('/user', (req, res) => {
-    res.status(201).json(req.body);
-} );
+app.use("/user", (req, res) => {
+  
+  if ( ValidateEmail(req.body.testapp.email) == false) {
+    return res.status(400).json({ message: "Invaild Email" });
+  }
+  if ( ValidateName(req.body.testapp.name) == false) {
+    console.log("in");
+    return res.status(400).json({ message: "  Enter Only Alphabets" });
+  }
+  if ( ValidateMobiles(req.body.testapp.mobile) == false) {
+    console.log("in");
+    return res.status(400).json({ message: " Invalid Mobile Number" });
+  }
+  res.status(201).json(req.body);
+});
 
 app.listen(PORT, () => {
   console.log("Servber Is Connected");
---
+++ backend/utils/ReqValidation.js
@@ -0,0 +1,29 @@
+const emailRegex =/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
+const onlyChar = /^(\s{0,1}[A-Za-z])+$/;
+const onlyNum = /^\d{10}$/;
+
+const ValidateEmail = (email) => {
+  
+  if (!email || !emailRegex.test(email)) return false;
+  
+};
+
+const ValidatePassword = (password) => {};
+
+const ValidateMobiles = (Mobile) => {
+    if (!onlyNum.test(Mobile)) return false;
+
+};
+
+const ValidateName = (Name) => {
+
+  if (!onlyChar.test(Name)) return false;
+};
+
+module.exports = {
+  ValidateEmail,
+  ValidateName,
+  ValidatePassword,
+  GenrateResposne,
+  ValidateMobiles
+};
